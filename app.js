04c11e6c447ac2e25d5a437bb22a1f8e88cfd0b3
labeeb-ap <138503106+labeeb-ap@users.noreply.github.com>
2024-04-17 14:59:29 +0530

    ok

---
+++ app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello"));
+
+// Register user routes
+app.use("/user", userRoutes);
+
+// Connect to MongoDB
+mongoose.connect("mongodb+srv://labeebap446:JxNN2Fk494gbplqj@cluster0.ovqhyfr.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB connected");
+    // Start the server after successful database connection
+    app.listen(4000, () => {
+        console.log("Server started");
+    });
+}).catch(err => {
+    console.error("Error connecting to MongoDB:", err);
+    // Exit the process if unable to connect to MongoDB
+    process.exit(1);
+});
