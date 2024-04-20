c751df3b6c29e4df5629250ebd55b207e71b96b7
MOHAMMED LABEEB A P <labeebap446@gmail.com>
2024-04-20 22:16:00 +0530

    Update wardRoutes.js

--- routes/wardRoutes.js
+++ routes/wardRoutes.js
@@ -8,33 +8,31 @@ const router = express.Router();
 //ward login
 router.post('/wardlogin', async (req, res) => {
   try {
-    console.log(req.body)
     const { username, password } = req.body;
-    console.log('Username:',username);
-    console.log('Password:',password);
+    console.log('Username:', username);
+    console.log('Password:', password);
     const user = await WardMembers.findOne({ username, password });
    
     if (user) {
-      console.log('exist');
-      console.log(user);
+      console.log('Ward member exists:', user);
       res.json({
         "code": 200,
         "data": user
-      })
+      });
     } else {
-      res.json({
+      console.log('Ward member does not exist');
+      res.status(404).json({
         "code": 100,
         "message": "Ward Member does not exist"
-      })
-      console.log('not exist');
+      });
     }
 
   } catch (error) {
-    // If an error occurs, send an error response
-    console.error('Error in authority registration:', error);
+    console.error('Error in ward member login:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+
 
 //User login
 router.post('/userlogin', async (req, res) => {
@@ -64,6 +62,45 @@ router.post('/userlogin', async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 });
+
+
+
+router.post('/userRequests', async (req, res) => {
+  try {
+    const { state, district, localgovernment, wardNo } = req.body;
+    console.log('State:', state);
+    console.log('District:', district);
+    console.log('Local Government:', localgovernment);
+    console.log('Ward Number:', wardNo);
+
+    // Assuming you have a MongoDB model named VerifiedUsers
+    const users = await RequestedUsers.find({ 
+      state,
+      district,
+      localgovernment,
+      wardNo
+    });
+
+    if (users && users.length > 0) {
+      console.log('Users found:', users);
+      res.json({
+        code: 200,
+        data: users
+      });
+    } else {
+      console.log('No users found');
+      res.json({
+        code: 100,
+        message: 'No users found with the provided details'
+      });
+    }
+  } catch (error) {
+    // If an error occurs, send an error response
+    console.error('Error in fetching user requests:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 
 
 
