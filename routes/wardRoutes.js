d9efa31ac1f52ec460985dd273a4d158c2398acd
niss-shana <134854766+niss-shana@users.noreply.github.com>
2024-04-20 10:51:13 +0530

    Update wardRoutes.js

--- routes/wardRoutes.js
+++ routes/wardRoutes.js
@@ -41,14 +41,17 @@ router.post('/userlogin', async (req, res) => {
   try {
     const { username, password } = req.body;
     console.log('username:',username);
+    console.log('Password:',password);
     const user = await VerifiedUsers.findOne({ username, password });
     if (user) {
+      console.log('exist')
       console.log(user);
       res.json({
         "code": 200,
         "data": user
       })
     } else {
+      console.log('not exist')
       res.json({
         "code": 100,
         "message": "User does not exist"
