5cd46a81f888060455f67b6c37a6ab7592ef8dda
niss-shana <134854766+niss-shana@users.noreply.github.com>
2024-05-01 12:58:25 +0530

    add

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -34,5 +34,29 @@ router.post('/request-user', async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+router.post('/details', async (req, res) => {
+    try {
+      console.log("Complete Details of residents");
+      const { wardid} = req.body;
+      console.log(req.body)
+      
+      
+      const users = await VerifiedUsers.find({wardid:wardid });
+      console.log(users)
+      console.log(users.length);
+      if (users.length > 0) {
+        res.status(200).json({ success: true, message:'Message found',users });
+      } else{
+        console.log('No Message found for the user:', userMsg);
+        res.status(404).json({ success: false, message: 'No Message found for the user' });
+      }
+    } catch (error) {
+      console.error('Error fetching user message:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
+
 
 export default router;
+
